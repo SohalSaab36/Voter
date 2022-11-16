e0413c26c6ee0337520b8fa0a6eeb8fa5be3a548
SohalSaab36 <96253854+SohalSaab36@users.noreply.github.com>
2022-11-16 21:52:49 +0530

    Base v1

---
+++ voter.py
@@ -0,0 +1,32 @@
+NAME =    []
+DB = {}
+B = 0
+def vote():
+         print(f"candidate list {NAME}")
+         IN = input("CAST YOUR VOTE: ").lower()
+         DB.update({IN : DB.get(IN)+1})
+         ASK =    input("more voters ?(y/n) :    ").lower()
+         if ASK ==    "y":
+             vote()
+         elif ASK ==    "n":
+             exit()
+         elif ASK ==    "key":
+             print(DB)
+         else:
+             vote()
+ 
+def create():
+     A =    input("which voter to register: ")
+     NAME.append(A)
+     print(NAME) 
+     DB[A] =    0
+     B =    input("do you have more(y/n): ")
+     if B == "y":
+         B = 0
+         create()
+     elif B ==    "n":
+         vote()
+     else:
+         print("error")
+create()
+
